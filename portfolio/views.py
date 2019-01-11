309626c846c0a5168282f7982a61ae7f46621514
AndreFagereng <andre_fagereng@hotmail.com>
2019-01-11 16:21:17 +0100

    Updated about

--- portfolio/views.py
+++ portfolio/views.py
@@ -7,14 +7,17 @@ def index(request):
 	try:
 		repository_data = api.get_response('https://api.github.com/users/AndreFagereng/repos')
 		data = api.list_view(repository_data)
-	
+		
+		data = {
+		'data': data
+		}
+		
+		return render(request, 'portfolio/portfolio.html', data)	
 	except:
 		pass
 	
-	data = {
-		'data': data
-	}
-	return render(request, 'portfolio/portfolio.html', data)
+	
+	
 
 def about(request):
 	return render(request, 'portfolio/about.html')
